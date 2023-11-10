fc2397710ec1cdd681dd6cf9ceb0f0bd600620b6
imrane sarsri <imransarsi.ii@gmail.com>
2023-11-10 16:41:57 +0100

    Finish a task Maquittage-Prototype

--- Branche-Technique/Maquittage-Prototype/Maquittage-version-php/MaquittagePrototype.md
+++
@@ -1,1 +0,0 @@
-# Maquittage Prototype
--- Branche-Technique/Maquittage-Prototype/Maquittage-version-php/Sections/footer.php
+++
@@ -1,5 +0,0 @@
-<!-- /.content-wrapper -->
-<footer class="main-footer text-center">
-    <strong>Copyright &copy; 2023-2024 CNMH.</strong>
-    All rights reserved.
-</footer>
